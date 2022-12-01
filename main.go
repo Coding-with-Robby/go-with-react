1c353eb08eaa65b10514e01f57928653044bdff1
Robby Klein <hello@robbyk.com>
2022-11-30 17:46:15 -0800

    starting code

--- main.go
+++ main.go
@@ -32,14 +32,7 @@ func main() {
 	app.Get("/api/tasks/:id", controllers.FetchTask)
 	app.Delete("/api/tasks/:id", controllers.DeleteTask)
 
-	frontendRoutes := []string{
-		"/",
-		"/about",
-	}
-
-	for _, route := range frontendRoutes {
-		app.Get(route, controllers.Home)
-	}
+	app.Get("/", controllers.Home)
 
 	// Start app
 	app.Listen(":" + os.Getenv("PORT"))
